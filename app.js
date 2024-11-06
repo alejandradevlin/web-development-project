4e5a9a8a3cf2319165ccee53631bf520d4e999bf
alejandradevlin <143341706+alejandradevlin@users.noreply.github.com>
2024-11-05 20:20:29 -0500

    h

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 app.post('/submit-form', (req, res) => {
     const username = req.body.username; // access form data
     // Add validation logic here
-    res.send(`Username is $adevlin3`);
+    res.send(`Thanks!`);
 });
 
 app.listen(port, () => {
