e82a1c0ef50c82b51fc81c06379cff92af53c8d4
alejandradevlin <143341706+alejandradevlin@users.noreply.github.com>
2024-11-05 21:15:18 -0500

    l

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); //create server using the express module in nodejs
+const app = express(); //create an instance of express 'app'
+const port = 3000; //set environment variable 'port' to 3000, this is for localhost3000:
 
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded, middle ware
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+app.get('/', (req, res) => { //get action, retrieve file from server 
+    res.sendFile(__dirname + '/index.html'); // to send HTML file on GET request
 });
 
-app.post('/submit-form', (req, res) => {
-    const comments = req.body.comments; // access form data
-    // Add validation logic here
+app.post('/submit-form', (req, res) => {  //post action, insert resource into the server, req will store incoming objects, res will be the response sent to the server ('Thanks!')
+    const comments = req.body.comments; //  to access form data
     res.send(`Thanks!`);
 });
 
-app.listen(port, () => {
+app.listen(port, () => { //listening on specified port, in this case 3000
     console.log(`Server running on http://localhost:${port}`);
 });
