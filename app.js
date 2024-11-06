93954585f07e84bdeffa14e3bc9365aca3f40819
alejandradevlin <143341706+alejandradevlin@users.noreply.github.com>
2024-11-05 20:45:13 -0500

    l

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/submit-form', (req, res) => {
-    const username = req.body.username; // access form data
+    const comments = req.body.comments; // access form data
     // Add validation logic here
     res.send(`Thanks!`);
 });
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function validateForm() { //code made with W3 Javascript Form Validation Module https://www.w3schools.com/js/js_validation.asp
+/**function validateForm() { //code made with W3 Javascript Form Validation Module https://www.w3schools.com/js/js_validation.asp
     let x = document.forms["comment"]["fname"].value;
     if (x == "") {
       alert("The field is blank. Please Type something!");
@@ -7,4 +7,4 @@ function validateForm() { //code made with W3 Javascript Form Validation Module 
         alert("Thanks!")
       }
 
-  } 
+  } */
